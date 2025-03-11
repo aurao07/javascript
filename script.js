1b63e0a9c9349a73f94eba6277a162688ce8f0c6
Aura.Orantes <aorantes@gradcenter.cuny.edu>
2025-03-11 13:13:52 -0400

    Fifth Commit

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //alert('My project is going to be awesome!');
+$("#noShow").hide();
+$("h1").html("The Best Jokes Ever!");
+$("#Jokes").append("<p>Q. What do you call a fake noodle? <br />A. An impasta</p>");
+$("#toggleJokes").css({
+    "background-color": "orange",
+    "color": "white",
+    "font-size": "20px",
+    "padding": "10px",
+    "border-radius": "5px",
+    "border": "2px solid black",
+    "margin": "10px"
+});
+$("#toggleJokes").click(function(){
+    $("#Jokes").toggle();
+  });
+
+$("#image").css({
+    "border-radius": "50%",
+    "border": "2px solid black",
+})

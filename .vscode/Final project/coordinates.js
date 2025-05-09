91fdfb397fd1b3e9316b66b0ac085a4aa98ad40b
Aura.Orantes <aorantes@gradcenter.cuny.edu>
2025-05-08 21:19:27 -0400

    Second commit

---
+++ .vscode/Final project/coordinates.js
@@ -0,0 +1,187 @@
+const nycLawyers= {
+  "type": "FeatureCollection",
+  "features": [
+ {
+   "type": "Feature",
+   "geometry": {
+      "type": "Point",
+      "coordinates":  [ 40.704656401680474, -74.01266526490303 ]
+   },
+   "properties": {
+   "Law Office":"The Law Offices of Grinberg & Segal, PLLC",
+   "Rating":4.6,
+   "Neighborhood":"Lower Manhattan",
+   "Consultation fee":"$260",
+   "Maps":"https://maps.app.goo.gl/79soeVUHA8NwQTte8"
+   }
+ },
+ {
+   "type": "Feature",
+   "geometry": {
+      "type": "Point",
+      "coordinates":  [ 40.71401794941632, -74.01475397465929 ]
+   },
+   "properties": {
+   "Law Office":"Zohar Law, PLLC",
+   "Rating":4.9,
+   "Neighborhood":"Lower Manhattan",
+   "Consultation fee":"Free",
+   "Maps":"https://maps.app.goo.gl/rAVfBZV2e5SKTg4A8"
+   }
+ },
+ {
+   "type": "Feature",
+   "geometry": {
+      "type": "Point",
+      "coordinates":  [ 40.70812814279496, -74.00543042103766 ]
+   },
+   "properties": {
+   "Lawyer":"The Law Firm of Moumita Rahman",
+   "Rating":4.8,
+   "Neighborhood":"Lower Manhattan",
+   "Consultation Fee":"$150",
+   "Maps":"https://maps.app.goo.gl/4kDkPKtkRW7tTtcd6"
+   }
+ },
+   {
+   "type": "Feature",
+   "geometry": {
+      "type": "Point",
+      "coordinates":  [ 40.59113224156696, -73.9681798967336 ]
+   },
+   "properties": {
+   "Lawyer":"JT Law Firm",
+   "Rating":5.0,
+   "Neighborhood":"Sheepshead Bay",
+   "Consultation Fee":"Free",
+   "Maps":"https://maps.app.goo.gl/QcEb67XXGYWRnmgU6"
+   }
+ },
+   {
+   "type": "Feature",
+   "geometry": {
+      "type": "Point",
+      "coordinates":  [ 40.59193480682943, -73.9605900741473 ]
+   },
+   "properties": {
+   "Lawyer":"Law Offices of Robert Tsigler, PLLC",
+   "Rating":4.6,
+   "Neighborhood":"Sheepshead Bay"
+   "Consultation Fee":"Unknown",
+   "Maps":"https://maps.app.goo.gl/p8Lkq38WUMsY5K2v5"
+   }
+ },
+    {
+   "type": "Feature",
+   "geometry": {
+      "type": "Point",
+      "coordinates":  [ 40.57814754631741, -73.96173603872106 ]
+   },
+   "properties": {
+   "Lawyer":"Modern Law Group, P.C.",
+   "Rating":4.8,
+   "Neighborhood":"Sheepshead Bay"
+   "Consultation Fee":"Unknown",
+   "Maps":"https://maps.app.goo.gl/vJr1sAPvbmQE4uDGA"
+   }
+ },
+   {
+   "type": "Feature",
+   "geometry": {
+      "type": "Point",
+      "coordinates":  [ 40.607016038105314, -73.96336732044416 ]
+   },
+   "properties": {
+   "Lawyer":"Law Firm of Victoria Ledeneva, Esq.",
+   "Rating":5.0,
+   "Neighborhood":"Sheepshead Bay"
+   "Consultation Fee":"$150",
+   "Maps":"https://maps.app.goo.gl/Npcntt9VFkY3YGTN9"
+   }
+ },
+  {
+   "type": "Feature",
+   "geometry": {
+      "type": "Point",
+      "coordinates":  [ 40.70301456985538, -74.0139039221529 ]
+   },
+   "properties": {
+   "Lawyer":"Katona & Associates, PLLC",
+   "Rating":5.0,
+   "Neighborhood":"Lower Manhattan"
+   "Consultation Fee":"Unknown",
+   "Maps":"https://maps.app.goo.gl/HSFJu3GYTa9ZRTfYA"
+   }
+ },
+   {
+   "type": "Feature",
+   "geometry": {
+      "type": "Point",
+      "coordinates":  [ 40.76441217869557, -73.91730674967023 ]
+   },
+   "properties": {
+   "Lawyer":"Vasilou Law",
+   "Rating":5.0,
+   "Neighborhood":"Astoria"
+   "Consultation Fee":"Free",
+   "Maps":"https://maps.app.goo.gl/n4wFSenLaekU5pUWA"
+   }
+ },
+   {
+   "type": "Feature",
+   "geometry": {
+      "type": "Point",
+      "coordinates":  [ 40.74833867660017, -73.98502986562085 ]
+   },
+   "properties": {
+   "Lawyer":"Buschkin Law Firm",
+   "Rating":"N/A",
+   "Neighborhood":"Murray Hill"
+   "Consultation Fee":"Unknown",
+   "Maps":"https://maps.app.goo.gl/WVM8Pvkjfkc6KxJ98"
+   }
+ },
+   {
+   "type": "Feature",
+   "geometry": {
+      "type": "Point",
+      "coordinates":  [ 40.763807724921534, -73.97361357301247 ]
+   },
+   "properties": {
+   "Lawyer":"The Law Office of Edit Stelczner",
+   "Rating":"5.0",
+   "Neighborhood":"Midtown"
+   "Consultation Fee":"Unknown",
+   "Maps":"https://maps.app.goo.gl/rVYmoXWbRdBEbszKA"
+   }
+ },
+    {
+   "type": "Feature",
+   "geometry": {
+      "type": "Point",
+      "coordinates":  [ 40.74882713015919, -73.98639597301248 ]
+   },
+   "properties": {
+   "Lawyer":"Law Office of Michael H. Joseph, PLLC",
+   "Rating":"4.7",
+   "Neighborhood":"Chelsea"
+   "Consultation Fee":"Free",
+   "Maps":"https://maps.app.goo.gl/YgGJhHg4jNb6uqwp6"
+   }
+ },
+    {
+   "type": "Feature",
+   "geometry": {
+      "type": "Point",
+      "coordinates":  [ 40.74882713015919, -73.98639597301248 ]
+   },
+   "properties": {
+   "Lawyer":"Law Office of Michael H. Joseph, PLLC",
+   "Rating":"4.7",
+   "Neighborhood":"Chelsea"
+   "Consultation Fee":"Free",
+   "Maps":"https://maps.app.goo.gl/YgGJhHg4jNb6uqwp6"
+   }
+ },
+]
+}
